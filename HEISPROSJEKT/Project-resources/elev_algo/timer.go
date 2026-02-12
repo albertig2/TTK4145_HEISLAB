e50cd70a797ad810258344d44dc0a1d01f26f57f
Albertine <albertine.gjos@orbitntnu.com>
2026-02-12 19:26:57 +0100

    Translated timer

--- HEISPROSJEKT/Project-resources/elev_algo/timer.go
+++ HEISPROSJEKT/Project-resources/elev_algo/timer.go
@@ -1,47 +1,19 @@
-package timer 
-import ("time")
+import "time"
 
+var (
+	timerEndTime time.Time
+	timerActive  bool
+)
 
-func get_wall_time() float64 {
-    var time struct {
-        tv_sec  int64
-        tv_usec int64  
-    }
-    gettimeofday(&time, nil)
-    return float64(time.tv_sec) + float64(time.tv_usec)*0.000001
+func timerStart(d time.Duration) {
+	timerEndTime = time.Now().Add(d)
+	timerActive = true
 }
 
-float64 timerEndTime;
-bool timerActive;
-
-func timer_start(duration float64) {
-    timerEndTime = get_wall_time() + duration
-    timerActive = true
+func timerStop() {
+	timerActive = false
 }
 
-func timer_stop() {
-    timerActive = false
+func timerTimedOut() bool {
+	return timerActive && time.Now().After(timerEndTime)
 }
-
-func timer_timedOut() bool {
-    return timerActive && get_wall_time() > timerEndTime 
-}
-
-static  double          timerEndTime;
-static  int             timerActive;
-
-void timer_start(double duration){
-    timerEndTime    = get_wall_time() + duration;
-    timerActive     = 1;
-}
-
-void timer_stop(void){
-    timerActive = 0;
-}
-
-int timer_timedOut(void){
-    return (timerActive  &&  get_wall_time() > timerEndTime);
-}
-
-
-
