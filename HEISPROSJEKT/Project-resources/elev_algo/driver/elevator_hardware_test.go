8c39d9b596181ef9a61299b15a0725bb71ab014d
Mathilde <mathilde.skaset.haar@gmail.com>
2026-02-15 17:54:19 +0100

    Fix conistensy issue with type names

--- HEISPROSJEKT/Project-resources/elev_algo/driver/elevator_hardware_test.go
+++ HEISPROSJEKT/Project-resources/elev_algo/driver/elevator_hardware_test.go
@@ -4,15 +4,15 @@ package driver
 
 
 func main() {
-    elevator_hardware_init() // add local IP å+ port when testing
+    elevator_hardware_init() // add local IP + port when testing
 
     for {
 
-	elevator_hardware_set_motor_direction(MDIR_DOWN);
+	elevator_hardware_set_motor_direction(D_DOWN)
 
 	for(elevator_hardware_get_floor_sensor_signal() != 0) {}
 
-	elevator_hardware_set_motor_direction(MDIR_UP);
+	elevator_hardware_set_motor_direction(D_UP)
 
 	for(elevator_hardware_get_floor_sensor_signal() != 3) {}
     }
