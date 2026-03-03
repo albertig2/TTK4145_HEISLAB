73c96022a89d09e59e9ff04e549d09ef9d221ea9
Mathilde <mathilde.skaset.haar@gmail.com>
2026-03-03 21:39:03 +0100

    Renamed some functions, + code cleanup

--- HEISPROSJEKT/elevator/Hardware/hardwareSafety.go
+++ HEISPROSJEKT/elevator/Hardware/hardwareSafety.go
@@ -27,13 +27,12 @@ var currentElevatorState elevatorState = IDLE
 var _nextMotorDirection elevio.MotorDirection = elevio.MD_Up
 var _lastKnownDirection elevio.MotorDirection = elevio.MD_Stop
 
-func MotorDriection(motorDirection chan elevio.MotorDirection) {
+func updateMotorDirection(motorDirection chan elevio.MotorDirection) {
 	for {
 		d := <-motorDirection
 		println("Motordirection:", d)
 
 		if d != elevio.MD_Stop {
-
 			_lastKnownDirection = d
 		}
 
@@ -42,37 +41,6 @@ func MotorDriection(motorDirection chan elevio.MotorDirection) {
 }
 
 
-// func OpenDoor(openDoorChannel chan bool, motorDirection chan elevio.MotorDirection) {
-
-// 	_doorTimer := time.NewTimer(_doorOpenTime)
-// 	_doorTimer.Stop()
-
-// 	for {
-// 		select {
-// 		case openDoor := <-openDoorChannel:
-// 			fmt.Println("Door open was triggerd")
-// 			if openDoor && !(isBetweenFloors()) {
-// 				fmt.Println("Door open loop was triggerd")
-// 				motorDirection <- elevio.MD_Stop
-// 				_doorTimer.Reset(_doorOpenTime)
-// 				_doorIsOpen = true
-// 				elevio.SetDoorOpenLamp(true)
-// 			} else {
-// 				//do nothing if false
-// 			}
-
-// 		case <-_doorTimer.C:
-// 			fmt.Println("Timeout was triggerd")
-// 			_doorIsOpen = false
-// 			elevio.SetDoorOpenLamp(false)
-// 			motorDirection <- (_lastKnownDirection)
-// 		}
-// 	}
-// }
-
-// func manegOrderLight(){
-
-// }
 
 func isBetweenFloors() bool {
 	currentFloor := elevio.GetFloor()
@@ -114,7 +82,7 @@ func RunElevatorHardware(hardwareChannels ElevatorHardwareChannelsStruckt) {
 	doorTimer := time.NewTimer(_doorOpenTime)
 	doorTimer.Stop()
 
-	go MotorDriection(hardwareChannels.MotorDirectionChannel)
+	go updateMotorDirection(hardwareChannels.MotorDirectionChannel)
 
 	for {
 		select {
@@ -153,69 +121,3 @@ func RunElevatorHardware(hardwareChannels ElevatorHardwareChannelsStruckt) {
 		}
 	}
 }
-
-		// case state := <-hardwareChannels.ElevatorStateChannel:
-		// 	switch state {
-
-		// 	case IDLE:
-		// 		currentElevatorState = IDLE
-		// 		hardwareChannels.MotorDirectionChannel <- elevio.MD_Stop
-
-		// 	case MOVING:
-		// 		currentElevatorState = MOVING
-		// 		hardwareChannels.MotorDirectionChannel <- _nextMotorDirection
-		// 	case DOOROPEN:
-		// 		currentElevatorState = DOOROPEN
-
-		// 		hardwareChannels.MotorDirectionChannel <- elevio.MD_Stop
-		// 		fmt.Println("Door open was triggerd")
-		// 		doorTimer.Reset(_doorOpenTime)
-		// 		_doorIsOpen = true
-		// 		elevio.SetDoorOpenLamp(true)
-		// 	}
-
-// func HardwareSafetyFeatures(pollObstructionChannel chan bool, pollStopButtonChannel chan bool, openDoorChannel chan bool, motorDirection chan elevio.MotorDirection) {
-// 	for {
-// 		select {
-
-// 		case obstructionActivated := <-pollObstructionChannel:
-
-// 			// fmt. Println(obstructionActivated)
-// 			currentFloor := elevio.GetFloor()
-
-// 			if obstructionActivated {
-// 				if currentFloor != -1 {
-// 					fmt.Println("Obstruction was activated")
-// 					motorDirection <- elevio.MD_Stop
-// 					// openDoorChannel<- true
-
-// 				} else {
-// 					//Ignore input from obstruction if between floors/door is closed
-// 				}
-
-// 			} else {
-// 				fmt.Println("Obstruction was Reset")
-// 				motorDirection <- _lastKnownDirection
-// 			}
-
-// 		case stopActivated := <-pollStopButtonChannel:
-// 			fmt.Println(stopActivated)
-
-// 			if stopActivated {
-// 				fmt.Println("Stop was activated")
-
-// 				elevio.SetStopLamp(true)
-// 				motorDirection <- elevio.MD_Stop
-// 				TurnOffAllLights()
-
-// 			} else {
-// 				fmt.Println("Stop was Reset")
-
-// 				elevio.SetStopLamp(false)
-// 				motorDirection <- _lastKnownDirection
-
-// 			}
-
-// 		}
-// 	}
-// }
