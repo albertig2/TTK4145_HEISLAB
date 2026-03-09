99cfac6dd015a26f1dd5d144be1d9a5a9be284dd
Mathilde <mathilde.skaset.haar@gmail.com>
2026-03-09 11:30:52 +0100

    Move fsm functions around to crete a better file structure

--- HEISPROSJEKT/elevator/elevatorHardware/elevator.go
+++ HEISPROSJEKT/elevator/elevatorHardware/elevator.go
@@ -3,38 +3,22 @@ package elevatorHardware
 import (
 	"Driver-go/elevio"
 	"HEISPROSJEKT/elevatorConfig"
-	"time"
+	//"time"
 )
 
-func InitializeElevatorObject(id string) elevatorConfig.Elevator {
-
-	config := elevatorConfig.Config{
-		DoorOpenDuration_s: 3 * time.Second,
-	}
-
-	elevator := elevatorConfig.Elevator{
-		OwnId:     id,
-		Floor:     elevio.GetFloor(),
-		Direction: elevatorConfig.Stop,
-		Requests:  [elevatorConfig.N_FLOORS][elevatorConfig.N_BUTTONS]bool{},
-		Behavior:  elevatorConfig.Idle,
-		Config:    config,
-	}
-	return elevator
-}
-
-
-
-func Elevator_uninitialized() elevatorConfig.Elevator {
-	elevio.Init("localhost:15657", elevatorConfig.N_FLOORS)
-	es := elevatorConfig.Elevator{Floor: -1, Direction: elevatorConfig.Stop, Behavior: elevatorConfig.Idle, Config: elevatorConfig.Config{DoorOpenDuration_s: 3.0}}
-	return es
-}
+//------------------Sensors-------------------------------
 
 func ElevatorFloorSensor() int {
 	return elevio.GetFloor()
 }
 
+//-------------------motor------------------------------------
+func ElevatorMotorDirection(d elevatorConfig.Direction) {
+	elevio.SetMotorDirection(elevio.MotorDirection(d))
+}
+
+
+// ------------------Buttons--------------------------------
 func ElevatorRequestButton(f int, b elevatorConfig.Button) bool {
 	return elevio.GetButton((elevio.ButtonType)(b), f)
 }
@@ -47,7 +31,9 @@ func ElevatorObstruction() bool {
 	return elevio.GetObstruction()
 }
 
-func ElevatorFloorIndicator(f int) {
+//-------------------Lights----------------------------------
+
+func ElevatorFloorIndicatorLight(f int) {
 	elevio.SetFloorIndicator(f)
 }
 
@@ -62,9 +48,22 @@ func ElevatorDoorLight(v bool) {
 
 func ElevatorStopButtonLight(v bool) {
 	elevio.SetStopLamp(v)
-
 }
 
-func ElevatorMotorDirection(d elevatorConfig.Direction) {
-	elevio.SetMotorDirection(elevio.MotorDirection(d))
+func SetAllLights(elevator elevatorConfig.Elevator) {
+	for floor := 0; floor < elevatorConfig.N_FLOORS; floor++ {
+		for btn := 0; btn < elevatorConfig.N_BUTTONS; btn++ {
+			ElevatorRequestButtonLight(floor, elevatorConfig.Button(btn), elevator.Requests[floor][btn])
+		}
+	}
 }
+func TurnOffAllOrderLights() {
+	for floor := 0; floor < elevatorConfig.N_FLOORS; floor++ {
+		for button := elevio.ButtonType(0); button < 3; button++ {
+			elevio.SetButtonLamp(button, floor, false)
+		}
+	}
+}
+
+
+
---
+++ HEISPROSJEKT/elevator/elevatorHardware/elevatorFsmEventHandlers.go
@@ -0,0 +1,260 @@
+package elevatorHardware
+
+
+import (
+	"Driver-go/elevio"
+	"HEISPROSJEKT/debuggingHelpers"
+	"HEISPROSJEKT/elevatorConfig"
+	"fmt"
+	"time"
+)
+
+
+func InitElevatorBetweenFloors(elevator *elevatorConfig.Elevator) {
+	ElevatorMotorDirection(elevatorConfig.Down)
+	elevator.Direction = elevatorConfig.Down
+	elevator.Behavior = elevatorConfig.Moving
+}
+
+func InitializeElevator(id string) elevatorConfig.Elevator {
+
+	config := elevatorConfig.Config{
+		DoorOpenDuration_s: 3 * time.Second,
+	}
+
+	elevator := elevatorConfig.Elevator{
+		OwnId:     id,
+		Floor:     elevio.GetFloor(),
+		Direction: elevatorConfig.Stop,
+		Requests:  [elevatorConfig.N_FLOORS][elevatorConfig.N_BUTTONS]bool{},
+		Behavior:  elevatorConfig.Idle,
+		Config:    config,
+	}
+	return elevator
+}
+
+func InitElevatorHardwareChannels() elevatorConfig.ElevatorHardwareChannelsStruckt {
+
+	hardwareChannels := elevatorConfig.ElevatorHardwareChannelsStruckt{
+		PollOrderButtonsChannel: make(chan elevio.ButtonEvent),
+		PollObstructionChannel:  make(chan bool),
+		PollStopButtonChannel:   make(chan bool),
+		FloorSensorChannel:      make(chan int),
+		DoorOpenChannel:         make(chan bool),
+		MotorDirectionChannel:   make(chan elevatorConfig.Direction),
+		ElevatorObjectChannel:   make(chan elevatorConfig.Elevator),
+	}
+	return hardwareChannels
+}
+
+// small initialisation sequence to put the elevator in a known behavior
+// small initialisation sequence to put elevator in a known state
+func InitElevatorHardware(elevator *elevatorConfig.Elevator) {
+
+	TurnOffAllOrderLights()
+	elevio.SetDoorOpenLamp(false)
+	elevio.SetStopLamp(false)
+	InitElevatorBetweenFloors(elevator)
+
+}
+
+func updateMotorDirection(motorDirection chan elevatorConfig.Direction) {
+	for {
+		d := <-motorDirection
+
+		println("Motordirection:", d)
+
+		elevio.SetMotorDirection(elevio.MotorDirection(d))
+	}
+}
+func isBetweenFloors() bool {
+	currentFloor := elevio.GetFloor()
+	if currentFloor != -1 {
+		return false
+	} else {
+		return true
+	}
+
+}
+
+func HandleOnFloorArrival(elevator *elevatorConfig.Elevator, doorTimer *time.Timer, newFloor int, MotorDirectionChannel chan elevatorConfig.Direction) {
+	fmt.Println("Elevator arrived at:", newFloor)
+	debuggingHelpers.Elevator_print(*elevator)
+
+	elevator.Floor = newFloor
+	ElevatorFloorIndicatorLight(elevator.Floor)
+
+	switch elevator.Behavior {
+	case elevatorConfig.Moving:
+		if RequestsShouldStop(*elevator) {
+
+			ElevatorMotorDirection(elevatorConfig.Stop)
+
+			//MotorDirectionChannel <- elevio.MD_Stop
+			OpenDoor(elevator, doorTimer, elevatorConfig.DOOR_OPEN_DURATION_S)
+			//Elevator_doorLight(true)
+			//*elevator = RequestsClearAtCurrentFloor(*elevator)
+			//timer.Timer_start(elevatorConfig.DOOR_OPEN_DURATION_S)
+			//SetAllLights(*elevator)
+			//elevator.Behavior = elevatorConfig.DoorOpen
+		}
+	default:
+		// nothing
+	}
+
+	fmt.Printf("\nNew state after HandleOnFloorArrival:\n")
+	debuggingHelpers.Elevator_print(*elevator)
+}
+
+func OpenDoor(elevator *elevatorConfig.Elevator, doorTimer *time.Timer, timeOpenSeconds time.Duration) {
+
+	if !isBetweenFloors() {
+
+		fmt.Println("Door Open")
+
+		elevio.SetDoorOpenLamp(true)
+		*elevator = RequestsClearAtCurrentFloor(*elevator)
+
+		doorTimer.Stop()
+		doorTimer.Reset(timeOpenSeconds)
+
+		SetAllLights(*elevator)
+		elevator.Behavior = elevatorConfig.DoorOpen
+
+	} else {
+		fmt.Println("Door was attempted opend in between floors")
+		//Should probably trigger some sort of error handelig or somthing here
+	}
+}
+
+func OnDoorTimeout(elevator *elevatorConfig.Elevator, doorTimer *time.Timer) {
+	fmt.Println("Door timeout")
+	debuggingHelpers.Elevator_print(*elevator)
+
+	switch elevator.Behavior {
+	case elevatorConfig.DoorOpen:
+		pair := Requests_chooseDirection(*elevator)
+		elevator.Direction = pair.Direction
+		elevator.Behavior = pair.behavior
+
+		switch elevator.Behavior {
+		case elevatorConfig.DoorOpen:
+			// timer.Timer_start(elevator.Config.DoorOpenDuration_s)
+			// *elevator = RequestsClearAtCurrentFloor(*elevator)
+			// SetAllLights(*elevator)
+			OpenDoor(elevator, doorTimer, elevatorConfig.DOOR_OPEN_DURATION_S)
+
+		case elevatorConfig.Moving, elevatorConfig.Idle:
+			ElevatorDoorLight(false)
+			ElevatorMotorDirection(elevator.Direction)
+		}
+	default:
+		// nothing
+	}
+
+	fmt.Printf("\nNew state after  OnDoorTimeout:\n")
+	debuggingHelpers.Elevator_print(*elevator)
+}
+
+func HandleRequestButtonPress(elevator *elevatorConfig.Elevator, doorTimer *time.Timer, btn_floor int, btn_type elevatorConfig.Button, MotorDirectionChannel chan elevatorConfig.Direction) {
+	fmt.Printf("\n\n%s(%d, %s)\n", "Recieved the following order:", btn_floor, elevatorConfig.ButtonToString(btn_type))
+	debuggingHelpers.Elevator_print(*elevator)
+
+	switch elevator.Behavior {
+	case elevatorConfig.DoorOpen:
+		if RequestsShouldClearImmediately(*elevator, btn_floor, btn_type) {
+			doorTimer.Stop()
+			doorTimer.Reset(elevatorConfig.DOOR_OPEN_DURATION_S)
+			//timer.Timer_start(elevatorConfig.DOOR_OPEN_DURATION_S)
+		} else {
+			elevator.Requests[btn_floor][btn_type] = true
+		}
+
+	case elevatorConfig.Moving:
+		elevator.Requests[btn_floor][btn_type] = true
+
+	case elevatorConfig.Idle:
+		elevator.Requests[btn_floor][btn_type] = true
+		pair := Requests_chooseDirection(*elevator)
+		elevator.Direction = pair.Direction
+		elevator.Behavior = pair.behavior
+
+		switch pair.behavior {
+
+		case elevatorConfig.DoorOpen:
+			// ElevatorDoorLight(true)
+			// timer.Timer_start(elevatorConfig.DOOR_OPEN_DURATION_S)
+			// *elevator = RequestsClearAtCurrentFloor(*elevator)
+			OpenDoor(elevator, doorTimer, elevatorConfig.DOOR_OPEN_DURATION_S)
+
+		case elevatorConfig.Moving:
+			ElevatorMotorDirection(elevator.Direction)
+
+		case elevatorConfig.Idle:
+			// Do nothing
+		}
+	}
+
+	SetAllLights(*elevator)
+
+	fmt.Printf("\nNew state after HandleRequestButtonPres:\n")
+	debuggingHelpers.Elevator_print(*elevator)
+}
+
+func HandleStopButtonActivated(elevator *elevatorConfig.Elevator, doorTimer *time.Timer, MotorDirectionChannel chan elevatorConfig.Direction) {
+	fmt.Println("Stop was activated")
+	TurnOffAllOrderLights()
+	elevio.SetStopLamp(true)
+
+	//MotorDirectionChannel <- elevatorConfig.Stop
+
+	switch elevator.Behavior {
+
+	case elevatorConfig.DoorOpen:
+		//If stop is triggerd while at a floor, the door is opend and keep open until stop is reset + 3 seconds more
+		OpenDoor(elevator, doorTimer, elevatorConfig.DOOR_OPEN_DURATION_S)
+		doorTimer.Stop()
+
+	case elevatorConfig.Moving:
+		ElevatorMotorDirection(elevatorConfig.Stop)
+
+	default:
+
+	}
+
+}
+
+func HandleStopButtonreset(elevator *elevatorConfig.Elevator, doorTimer *time.Timer, MotorDirectionChannel chan elevatorConfig.Direction) {
+	fmt.Println("Stop was activated")
+	TurnOffAllOrderLights()
+	elevio.SetStopLamp(true)
+
+	//MotorDirectionChannel <- elevatorConfig.Stop
+
+	switch elevator.Behavior {
+
+	case elevatorConfig.Moving, elevatorConfig.Idle:
+		InitElevatorBetweenFloors(elevator)
+
+	default:
+
+	}
+
+}
+
+func HandleObstructionActivated(elevator *elevatorConfig.Elevator, doorTimer *time.Timer, MotorDirectionChannel chan elevatorConfig.Direction) {
+
+	fmt.Println("Obstruction was activated")
+	//ElevatorMotorDirection(elevatorConfig.Stop)
+	//MotorDirectionChannel <- elevatorConfig.Stop
+
+	switch elevator.Behavior {
+
+	case elevatorConfig.DoorOpen:
+		doorTimer.Stop()
+
+	default:
+		//Do nothing if the door is not open
+	}
+
+}
