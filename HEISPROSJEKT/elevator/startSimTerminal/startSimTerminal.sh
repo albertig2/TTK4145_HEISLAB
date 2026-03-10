cfc001700cd44923c3ba8d14273dc13639336453
LarsL123 <Lars@familienlien.com>
2026-03-10 11:31:48 +0100

    system finished for opening six terminal in linux and windows

--- HEISPROSJEKT/elevator/startSimTerminal/startSimTerminal.sh
+++ HEISPROSJEKT/elevator/startSimTerminal/startSimTerminal.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+
+#for ((i=1;i<=6;i++))
+#do
+#    gnome-terminal &
+#    sleep 0.1
+#done
 
 #!/bin/bash
 
-for ((i=1;i<=6;i++))
-do
-    gnome-terminal &
+for ((i=1;i<=6;i++)); do
+    xterm &
     sleep 0.1
 done
 
-# for å gjøre kjørbar: chmod +x startSimTerminal.sh
+#linux
+# må ha: sudo apt install xterm
+# for å gjøre kjørbar: chmod +x startSimTerminal.sh (usikker på om denne er nødvedig)
 # for å kjøre den: ./startSimTerminal.sh
 
 
