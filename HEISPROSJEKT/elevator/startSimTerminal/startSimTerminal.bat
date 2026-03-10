84147ee798a8fe2cc17b5621b8692566cc4fbc9a
odinsmelling <147643398+odinsmelling@users.noreply.github.com>
2026-03-10 06:21:02 +0100

    made a script that opens 6 terminals for the simulator

---
+++ HEISPROSJEKT/elevator/startSimTerminal/startSimTerminal.bat
@@ -0,0 +1,7 @@
+@echo off
+for /L %%i in (1,1,6) do (
+    start cmd
+)
+
+@REM for å kjøre: .\startSimTerminal.bat
+
---
+++ HEISPROSJEKT/elevator/startSimTerminal/startSimTerminal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!/bin/bash
+
+for ((i=1;i<=6;i++))
+do
+    gnome-terminal &
+    sleep 0.1
+done
+
+# for å gjøre kjørbar: chmod +x startSimTerminal.sh
+# for å kjøre den: ./startSimTerminal.sh
+
+
