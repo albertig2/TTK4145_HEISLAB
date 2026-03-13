2972edebc627a0814ad02b25f1245d6c73cf935b
JonasFroiland <jonassfr@stud.ntnu.no>
2026-03-13 18:44:25 +0100

    Error messages...

--- HEISPROSJEKT/elevator/orderProtocol/orderRutine.go
+++ HEISPROSJEKT/elevator/orderProtocol/orderRutine.go
@@ -55,20 +55,24 @@ func RunOrder(
 	//ticker.Stop()
 	defer ticker.Stop()
 	for {
+		fmt.Println("Before select in order routine")
 		select {
 		case servicedorder := <-elevatorOrderChannels.ServicedOrderChannel:
-
+			fmt.Println("Before servidec order")
 			if servicedorder.Button == elevatorConfig.HallUp || servicedorder.Button == elevatorConfig.HallDown {
 				servicedHallOrders = append(servicedHallOrders, servicedorder)
 			} else if servicedorder.Button == elevatorConfig.Cab {
 				servicedCabOrders = append(servicedCabOrders, servicedorder)
 			}
+			fmt.Println("After serviced order")
 		case newOrder := <-elevatorOrderChannels.NewRecievedOrderChannel:
+			fmt.Println("Before new order")
 			if newOrder.Button == elevatorConfig.HallUp || newOrder.Button == elevatorConfig.HallDown {
 				newHallOrders = append(newHallOrders, newOrder)
 			} else if newOrder.Button == elevatorConfig.Cab {
 				newCabOrders = append(newCabOrders, newOrder)
 			}
+			fmt.Println("After new order")
 		// etterhvert kan denne fjernes
 		case UpdatedSystem = <-synchronisationChannels.UpdateElevatorSystem:
 			system = UpdatedSystem
@@ -77,15 +81,18 @@ func RunOrder(
 			HallRequestsForAllElevators[UpdatedPeerRequests.PeerID] = UpdatedPeerRequests.HallReqs
 			CabRequestsForAllElevators[UpdatedPeerRequests.PeerID] = UpdatedPeerRequests.CabReqs
 		case motorstop := <-hardwareChannel.MotorFailureChannel:
+			fmt.Printf("Received motor failure status: %v", motorstop)
 			paused = motorstop
 			if motorstop {
 				synchronisation.InitializeElevatorSystem(&system, id)
 				HallRequestsForAllElevators[id] = system.HallRequests
 				CabRequestsForAllElevators[id] = system.States[id].CabRequests
 			}
+			fmt.Printf("Paused status: %v", paused)
 		case <-ticker.C:
 
 			if !paused {
+				fmt.Println("Before ordering")
 				var sh, sc elevatorConfig.ButtonEvent
 				sh.Floor = -1
 				sc.Floor = -1
@@ -103,11 +110,13 @@ func RunOrder(
 				servicedCabOrders = servicedCabOrders[:0]
 				newHallOrders = newHallOrders[:0]
 				newCabOrders = newCabOrders[:0]
-
+				fmt.Println("After ordering and before updating system on channel")
 				synchronisationChannels.UpdateElevatorSystem <- system
+				fmt.Println("After sending ordering on channel")
 			}
 
 		}
+		fmt.Println("After selct in orderrutine")
 	}
 }
 
--- HEISPROSJEKT/elevator/synchronisation/synchroniseElevators.go
+++ HEISPROSJEKT/elevator/synchronisation/synchroniseElevators.go
@@ -9,6 +9,7 @@ import (
 	// "strconv"
 	"HEISPROSJEKT/debuggingHelpers"
 	"HEISPROSJEKT/elevatorConfig"
+	"fmt"
 
 	//"fmt"
 	"time"
@@ -39,7 +40,9 @@ func SynchroniseElevators(elevatorUpdates chan elevatorConfig.Elevator, synchron
 				HallReqs: hallRequestsForAllElevators[incommingBroadcast.OwnId],
 				CabReqs:  cabRequestsForAllElevators[incommingBroadcast.OwnId],
 			}
+			fmt.Println("Before sending the peer update on channel (matrices)")
 			synchronisationChannels.UpdatePeerRequests <- peerRequests
+			fmt.Println("after sending peer update on channel (matrices)")
 
 		case peerUpdate := <-synchronisationChannels.PeerUpdateChl:
 			filteredAlivePeers := []string{}
@@ -63,11 +66,12 @@ func SynchroniseElevators(elevatorUpdates chan elevatorConfig.Elevator, synchron
 			synchronisationChannels.BcastOutgoingMessagesChannel <- elevatorSystem
 			broadcastTicker.Reset(time.Second / 30)
 		}
-
+		fmt.Println("Before jevnlig update of the elevator system on channel")
 		select {
 		case synchronisationChannels.UpdateElevatorSystem <- elevatorSystem:
 		default:
 		}
+		fmt.Println("After jevnlig update of the elevator system on channel")
 	}
 
 	//run all synchronisation on events (like elevator fsm, run events on most sync channel?)
