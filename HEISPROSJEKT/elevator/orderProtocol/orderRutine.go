0ea8b78fa39412ec57753589a1e0adb1f2978e71
JonasFroiland <jonassfr@stud.ntnu.no>
2026-03-13 15:59:26 +0100

    test

--- HEISPROSJEKT/elevator/orderProtocol/orderRutine.go
+++ HEISPROSJEKT/elevator/orderProtocol/orderRutine.go
@@ -27,7 +27,7 @@ func orderRutine(system *synchronisation.ElevatorSystem,
 	TransitioningAllCabRequests(system, CabRequestTransitions, elevatorOrderChannels)
 
 }
-//test
+//test1
 func RunOrder(
 	id string,
 	elevatorOrderChannels elevatorConfig.ElevatorOrderChannelStruckt,
